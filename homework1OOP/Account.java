6d8722ab20326c275d596ccbb65c3f258bfb78d3
Artemiy Pilipchuk <100224557+Bils1na@users.noreply.github.com>
2023-10-26 09:56:10 +0300

    added task1

--- homework1OOP/Account.java
+++ homework1OOP/Account.java
@@ -1,5 +1,30 @@
 package homework1OOP;
 
 public class Account() {
-    
+    /**
+     * Это счет в банке.
+     * @param value Это значение передает сумму на счете. Параметр типа double.
+     */
+    private double balance;
+
+    public Account(double value) {
+        this.balance = value;
+    }
+
+    public Account() {
+        this(0);
+    }
+
+    public void put(double value) {
+        this.balance += value;
+    }
+
+    public void take(double value) {
+        this.balance -= value;
+    }
+
+    public String getAmount() {
+        return String.format("Balance: %s", balance);
+    }
+
 }
