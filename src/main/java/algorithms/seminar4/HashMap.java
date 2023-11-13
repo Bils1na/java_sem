77f977b466e0dcef30ea67b20352c958f3261338
Artemiy Pilipchuk <100224557+Bils1na@users.noreply.github.com>
2023-11-13 16:06:53 +0300

    added sem4

---
+++ src/main/java/algorithms/seminar4/HashMap.java
@@ -0,0 +1,18 @@
+package seminar4.algorithms;
+
+public class HashMap<K, V> {
+
+    
+
+    private class Node {
+        final K key;
+        V value;
+        final int hash;
+        Node next;
+
+        private Node(K key, V value, int hash, ) {
+
+        }
+    }
+
+}
