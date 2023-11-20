f812f20aa979d6d6acaf974b9f7710bb8cf26795
Artemiy Pilipchuk <100224557+Bils1na@users.noreply.github.com>
2023-11-20 12:18:18 +0300

    added lec5OOP

---
+++ src/main/java/lecture5OOP/Calculator.java
@@ -0,0 +1,10 @@
+package lecture5OOP;
+
+public class Calculator {
+    private int result;
+
+    public int sum(int x, int y) {
+        result = x + y;
+        return result;
+    }
+}
---
+++ src/main/java/lecture5OOP/Main.java
@@ -0,0 +1,8 @@
+package lecture5OOP;
+
+public class Main {
+    Calculator calculator = new Calculator();
+
+    calculator.sum(5, 10); // result == 15
+    
+}
