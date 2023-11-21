dd4d432a4bd766ecfc26a3e42eda4976c50a34e4
Artemiy Pilipchuk <100224557+Bils1na@users.noreply.github.com>
2023-11-21 20:04:15 +0300

    added calc

---
+++ src/main/java/seminar5OOP/Calculator.java
@@ -0,0 +1,9 @@
+package seminar5OOP;
+
+public class Calculator {
+
+    public int sum(int a, int b) {
+        return a + a; //error!
+    }
+
+}
--- src/main/java/seminar5OOP/Main.java
+++ src/main/java/seminar5OOP/Main.java
@@ -1,6 +1,19 @@
 package seminar5OOP;
 
 public class Main {
-
     
+    // MVC Model View Controller
+    // MVP Model View Presenter
+
+    // web site GB     -client
+    // mobile app GB   -client
+    // desktop app GB
+
+    // backend GB
+    // --- API
+    // --- Service
+    // --- DAO (Data Access Objects) Repository
+
+    // db GB
+
 }
