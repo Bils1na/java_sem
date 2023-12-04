6992277a4671b3fea34982d362dff4c565ad39bd
bilsina <bils1n@icloud.com>
2023-12-04 16:26:15 +0300

    added hm6OOP

--- src/main/java/homework6OOP/Homework.java
+++ src/main/java/homework6OOP/Homework.java
@@ -1,18 +1,20 @@
 package homework6OOP;
 
+
 public class Homework {
 
     public static void main(String[] args) {
-
+        ReportService reportService = new ReportService();
+        byte[] jsonReport = reportService.createReport(new Document("1", ReportType.JSON));
     }
 
-    static class ReportService {
+    private static class ReportService {
 
-        public byte[] createReport(Document document, String reportType) {
+        public byte[] createReport(Document document) {
             // reportType = {"pdf-1", "json", "xml", ...}
             try {
-                System.out.printf("Отчет сформирован в формате %s.", reportType);
-                return null;
+                System.out.println("Отчет сформирован");
+                return document.getType().formatFile(document.getType());
             } catch (Exception e) {
             throw new UnsupportedOperationException();
             }
@@ -20,15 +22,33 @@ public class Homework {
 
     }
 
-    static class Document {
+    private static class Document {
         private long id;
         private String number;
+        private ReportType type;
+
+        public Document(String number, ReportType type) {
+            this.number = number;
+            this.type = type;
+        }
+
+        public ReportType getType() {
+            return type;
+        }
 
     }
 
-    enum ReportType {
-        PDF1, JSON, XML, DOCX, TXT
+    private enum ReportType {
+        PDF1, JSON, XML, DOCX, TXT;
+
+
+        public byte[] formatFile(ReportType type) {
+            // file format to type...
+            return null;
+        }
+
     }
+
 }
 
 
